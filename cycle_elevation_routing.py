a86bb226682c0af9ac9a4b70fcc18e4ec5ca5f69
dcs8888 <32004625+dcs8888@users.noreply.github.com>
2018-06-23 13:50:04 -0500

    Use image and json request
    Have both types of API's used for later reference

--- cycle_elevation_routing.py
+++ cycle_elevation_routing.py
@@ -1,5 +1,10 @@
 import requests
 import math
+from PIL import Image
+from io import BytesIO
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 """
 From
@@ -17,14 +22,24 @@ def main():
     z = 15
     x, y = deg2num(38.878103, -94.859009, z)
     
-    r = requests.get('https://tile.thunderforest.com/thunderforest.outdoors-v1.json?apikey=e25c01ab4aa14652b5bd3472c5e1f859')
+    r_img = requests.get('https://tile.thunderforest.com/cycle/' + str(z) + '/' + str(x) + '/' + str(y) + '.jpg90?apikey=e25c01ab4aa14652b5bd3472c5e1f859')
 
-    print(r.headers['Content-Type'])
-    print(r.status_code)
+    r_json = requests.get('https://tile.thunderforest.com/thunderforest.outdoors-v1.json?apikey=e25c01ab4aa14652b5bd3472c5e1f859')
     
-    if r.status_code == requests.codes.ok:
+    print(r_img.headers['Content-Type'])
+    print(r_img.status_code)
+    
+    print(r_json.headers['Content-Type'])
+    print(r_json.status_code)
+    
+    if r_img.status_code == requests.codes.ok:
         
-        print(r.json())
+        i = Image.open(BytesIO(r_img.content))
+        imgplot = plt.imshow(i)
+        plt.show()
+        
+    if r_json.status_code == requests.codes.ok:
+        print(r_json.json())
 
 if __name__ == "__main__":
    main()
