ae23e9ec861a0ba25b16261e4230449a2bf7998d
dcs8888 <32004625+dcs8888@users.noreply.github.com>
2018-06-24 20:40:57 -0500

    Add OSM data download
    Add OSM data download and save to xml file

--- cycle_elevation_routing.py
+++ cycle_elevation_routing.py
@@ -26,6 +26,7 @@ def main():
 
     r_json = requests.get('https://tile.thunderforest.com/thunderforest.outdoors-v1.json?apikey=e25c01ab4aa14652b5bd3472c5e1f859')
 
+    print('Request Vector Outdoors')
     print(r_json.headers['Content-Type'])
     print(r_json.status_code)
 
@@ -44,6 +45,7 @@ def main():
 
     r_img = requests.get('https://tile.thunderforest.com/cycle/' + str(z) + '/' + str(x) + '/' + str(y) + '.jpg90?apikey=e25c01ab4aa14652b5bd3472c5e1f859')
 
+    print('Request CYCLE IMG')
     print(r_img.headers['Content-Type'])
     print(r_img.status_code)
 
@@ -51,6 +53,20 @@ def main():
         i = Image.open(BytesIO(r_img.content))
         imgplot = plt.imshow(i)
         plt.show()
+
+    left = str(-94.840385)
+    right = str(-94.788114)
+    bottom = str(38.845563)
+    top = str(38.89488)
+    r_osm = requests.get('https://api.openstreetmap.org/api/0.6/map?bbox=' + left + ',' + bottom + ',' + right + ',' + top)
+    
+    print('Request OSM')
+    print(r_osm.headers['Content-Type'])
+    print(r_osm.status_code)
+    
+    if r_osm.status_code == requests.codes.ok:
+        with open('osm.xml', 'wb') as f_osm:
+            f_osm.write(r_osm.content)
 
 if __name__ == "__main__":
    main()
