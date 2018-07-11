0b9d5b3f77eb37aaeb78c73d6f855bcf836174c0
Dustin Shorter <dustin.shorter@garmin.com>
2018-07-11 10:34:56 -0500

    Create Prior List and Hist data
    Create priority list for a user and some historical data

--- user_profile.py
+++ user_profile.py
@@ -7,62 +7,126 @@ class User:
         self.user_id = user_id
 
 class UserProfiles:
-    user_list = []
+    user_list = dict()
 
-    def addUserList(self, user):
-        self.user_list.append(user)
+    def addNewUser(self, user, priorities):
+        self.user_list[user] = {'priority': priorities}
+        
+    def addUserRouteInfo(self, user, route_info):
+        self.user_list[user]['route_info'] = route_info
 
-    def getUserList(self):
+    def getUserPriorityList(self):
         return self.user_list
-
-def importData(users):
-    users_json_data = open('users.json').read()
-    users_pandas_data = pandas.read_json(users_json_data)
-
-    routes_json_data = open('routes.json').read()
-    routes_pandas_data = pandas.read_json(routes_json_data)
-
-    return users_pandas_data, routes_pandas_data
 
 def main():
 
-    current_user_id = 1691090622299
-
     users = UserProfiles()
-    users_pandas_data, routes_pandas_data = importData(users)
-
-    current_user = users_pandas_data.loc[users_pandas_data['UserID'] == current_user_id]
-    index = int(current_user.index.values)
-
-    priority_list = current_user['Priority']
-
-    first = routes_pandas_data.loc[(routes_pandas_data['ElevationAvg'].values <= current_user['ElevationAvg'].values) &
-                                   (routes_pandas_data['ElevationMax'].values <= current_user['ElevationMax'].values) &
-                                   (routes_pandas_data['ElevationMin'].values >= current_user['ElevationMin'].values) & 
-                                   (routes_pandas_data['TimeAvailable'].values <= current_user['TimeAvailable'].values)
-                                   ]
-
-    for item in priority_list[index].split(', '):
-        print(item)
-        final = first.loc[(first[item].values == current_user[item].values)]
+    priority_1234 = ['time', 'bike lanes', 'elevation', 'distance']
+    priority_1111 = ['bike lanes', 'time', 'distance', 'elevation']
+    
+    users.addNewUser(1234, priority_1234)
+    users.addNewUser(1111, priority_1111)
+    
+    users_list = users.getUserPriorityList()
         
-        final_route_id = final['RouteID'].values
-        print(final_route_id.size)
+    route_info_1234 = { 'elevation':      {'high':
+                                        {'weather':
+                                            {'clear':10, 'cloudy':12, 'rain':0, 'storm':0, 'snow':0, 'fog':0, 'windy':1},
+                                        'traffic':
+                                            {'low':5, 'medium':4, 'high':1}},
+                                'medium':
+                                        {'weather':
+                                            {'clear':16, 'cloudy':14, 'rain':0, 'storm':0, 'snow':0, 'fog':0, 'windy':1},
+                                        'traffic':
+                                            {'low':15, 'medium':12, 'high':22}},
+                                'low':
+                                        {'weather':
+                                            {'clear':25, 'cloudy':17, 'rain':0, 'storm':0, 'snow':0, 'fog':2, 'windy':2},
+                                        'traffic':
+                                            {'low':22, 'medium':18, 'high':12}}
+                                },
+             'bike lanes':      {'high':
+                                        {'traffic':
+                                            {'low':26, 'medium':15, 'high':5}},
+                                'medium':
+                                        {'traffic':
+                                            {'low':20, 'medium':14, 'high':8}},
+                                'low':
+                                        {'traffic':
+                                            {'low':5, 'medium':8, 'high':3}}
+                                },
+            'time consuming':   {'quick':
+                                        {'weather':
+                                            {'clear':14, 'cloudy':30, 'rain':1, 'storm':0, 'snow':0, 'fog':4, 'windy':3},
+                                        'temperature':
+                                            {'cold':3, 'cool':5, 'mild':21, 'warm':11, 'hot':4},
+                                        'time of day':
+                                            {'morning':5, 'afternoon':5, 'evening':39, 'night':2},
+                                        'weekday':
+                                            {'monday':2, 'tuesday':3, 'wednesday':2, 'thursday':2, 'friday':4, 'saturday':20, 'sunday':18}},
+                                'average':
+                                        {'weather':
+                                            {'clear':12, 'cloudy':15, 'rain':0, 'storm':0, 'snow':0, 'fog':2, 'windy':1},
+                                        'temperature':
+                                            {'cold':1, 'cool':2, 'mild':21, 'warm':8, 'hot':2},
+                                        'time of day':
+                                            {'morning':2, 'afternoon':2, 'evening':25, 'night':5},
+                                        'weekday':
+                                            {'monday':1, 'tuesday':2, 'wednesday':1, 'thursday':2, 'friday':2, 'saturday':11, 'sunday':13}},
+                                'very':
+                                        {'weather':
+                                            {'clear':7, 'cloudy':10, 'rain':0, 'storm':0, 'snow':0, 'fog':0, 'windy':0},
+                                        'temperature':
+                                            {'cold':0, 'cool':1, 'mild':14, 'warm':7, 'hot':0},
+                                        'time of day':
+                                            {'morning':0, 'afternoon':0, 'evening':14, 'night':1},
+                                        'weekday':
+                                            {'monday':1, 'tuesday':0, 'wednesday':0, 'thursday':0, 'friday':1, 'saturday':8, 'sunday':7}}
+                                }
+            }
+    
+    users.addUserRouteInfo(1234, route_info_1234)
+    
+    print(users_list)
 
-        if final_route_id.size == 0:
-            first.to_json('out.json')
-            print("Used First")
-            return
-        elif final_route_id.size == 1:
-            final.to_json('out.json')
-            print("Used Final")
-            return
+
+    # current_user_id = 1691090622299
+
+    # users = UserProfiles()
+    # users_pandas_data, routes_pandas_data = importData(users)
+
+    # current_user = users_pandas_data.loc[users_pandas_data['UserID'] == current_user_id]
+    # index = int(current_user.index.values)
+
+    # priority_list = current_user['Priority']
+
+    # first = routes_pandas_data.loc[(routes_pandas_data['ElevationAvg'].values <= current_user['ElevationAvg'].values) &
+                                   # (routes_pandas_data['ElevationMax'].values <= current_user['ElevationMax'].values) &
+                                   # (routes_pandas_data['ElevationMin'].values >= current_user['ElevationMin'].values) & 
+                                   # (routes_pandas_data['TimeAvailable'].values <= current_user['TimeAvailable'].values)
+                                   # ]
+
+    # for item in priority_list[index].split(', '):
+        # print(item)
+        # final = first.loc[(first[item].values == current_user[item].values)]
         
-        print("After Does first == final: " + str(first.equals(final)))
+        # final_route_id = final['RouteID'].values
+        # print(final_route_id.size)
 
-        first = final
-    print("Used Final Complete")
-    final.to_json('out.json')
+        # if final_route_id.size == 0:
+            # first.to_json('out.json')
+            # print("Used First")
+            # return
+        # elif final_route_id.size == 1:
+            # final.to_json('out.json')
+            # print("Used Final")
+            # return
+        
+        # print("After Does first == final: " + str(first.equals(final)))
+
+        # first = final
+    # print("Used Final Complete")
+    # final.to_json('out.json')
         
 if __name__ == "__main__":
    main()
